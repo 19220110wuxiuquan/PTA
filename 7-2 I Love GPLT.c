7f2dfb80adb6ae37637310fefa4417790945e8b9
wxq <114474518+19220110wuxiuquan@users.noreply.github.com>
2022-11-21 18:58:48 +0800

    Create 7-2 I Love GPLT.c

---
+++ 7-2 I Love GPLT.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    printf("I\n");
+    printf(" \n");
+    printf("L\n");
+    printf("o\n");
+    printf("v\n");
+    printf("e\n");
+    printf(" \n");
+    printf("G\n");
+    printf("P\n");
+    printf("L\n");
+    printf("T\n");
+}
