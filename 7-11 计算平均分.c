2438f62b9f7524cba762474d28f8b734d522342b
wxq <114474518+19220110wuxiuquan@users.noreply.github.com>
2022-11-21 19:02:45 +0800

    Create 7-11 计算平均分.c

---
+++ 7-11 计算平均分.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int math=87;
+    int eng=72;
+    int comp=93;
+    int average=(math+eng+comp)/3.0;
+    printf("math = %d, eng = %d, comp = %d, average = %d",math,eng,comp,average);
+    return 0;
+}
