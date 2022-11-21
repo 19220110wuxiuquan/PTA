cc878ce3ba6668303a75087d850c949fa7a6c1fd
wxq <114474518+19220110wuxiuquan@users.noreply.github.com>
2022-11-21 19:01:37 +0800

    Create 7-9 求整数均值.c

---
+++ 7-9 求整数均值.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int a,b,c,d;
+    int Sum;
+    double Average;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    Sum=a+b+c+d;
+    Average=Sum/4.0;
+    printf("Sum = %d; Average = %.1f",Sum,Average);
+    return 0;
+}
